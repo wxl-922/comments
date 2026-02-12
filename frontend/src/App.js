94deaec430418585496998944d43776d219a6618
wxl-922 <xwu46@buffalo.edu>
2026-02-12 01:59:59 -0500

    add comments sorting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,53 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [comments, setComments] = useState([]);
+  const [dropDown, setDropDown] = useState(false);
+  const [sortOrder, setSortOrder] = useState(() => {
+    return localStorage.getItem("sortOrder") || "id asc";
+  });
+
   useEffect(() => {
-    const listComments = async () => {
-      try {
-        const res = await fetch("http://127.0.0.1:5000/comments");
-        const jsonRes = await res.json();
-        const data = jsonRes.data;
-        setComments(data);
+    listComments();
+  }, []);
+
+  useEffect(() => {
+    sortComments(null);
+  }, [sortOrder]);
+
+  const listComments = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:5000/comments");
+      const jsonRes = await res.json();
+      const data = jsonRes.data;
+      sortComments(data);
       } catch (err) {
         console.error(err);
       }
-    }
-    listComments();
-  }, [])
+  }
+
+  const sortComments = (data) => {
+    const sortedComments = data === null ? [...comments] : [...data];
+    if (sortOrder === "id asc") sortedComments.sort((a, b) => a.id - b.id);
+    else if (sortOrder === "id desc") sortedComments.sort((a, b) => b.id - a.id);
+    else if (sortOrder === "date asc") sortedComments.sort((a, b) => new Date(a.date) - new Date(b.date));
+    else if (sortOrder === "date desc") sortedComments.sort((a, b) => new Date(b.date) - new Date(a.date));
+    localStorage.setItem("sortOrder", sortOrder);
+    setComments(sortedComments);
+  }
 
   return (
     <div className="App">
+      <div className={"dropdown-outer"}>
+        <button className={"dropdown-btn"} onClick={(e) => setDropDown(!dropDown)}>Sort By</button>
+        {dropDown && (
+          <div className={"dropdown-inner"}>
+            <button className={"dropdown-btn"} onClick={(e) => setSortOrder("id asc")}>ID Asc</button><br/>
+            <button className={"dropdown-btn"} onClick={(e) => setSortOrder("id desc")}>ID Desc</button><br/>
+            <button className={"dropdown-btn"} onClick={(e) => setSortOrder("date asc")}>Oldest</button><br/>
+            <button className={"dropdown-btn"} onClick={(e) => setSortOrder("date desc")}>Newest</button>
+          </div>
+        )}
+      </div>
       <ul>
         {comments.map(comment =>
           <li key={comment.id}>
